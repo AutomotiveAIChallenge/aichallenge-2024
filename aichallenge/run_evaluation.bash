87d633f264678f6b6bb37858e7a68e535484263c
taikitanaka3 <65527974+taikitanaka3@users.noreply.github.com>
2024-05-20 19:04:18 +0900

    chore: add window control (#7)
    * chore: add window control
    
    * update option order and remove debug command
    
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>
    
    ---------
    
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>
    Co-authored-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- aichallenge/run_evaluation.bash
+++ aichallenge/run_evaluation.bash
@@ -17,6 +17,9 @@ echo "Start AWSIM"
 PID_AWSIM=$!
 sleep 20
 
+# Move AWSIM window
+wmctrl -r "AWSIM" -e 0,0,0,960,1043
+
 # Start Autoware
 echo "Start Autoware"
 ros2 launch aichallenge_system_launch aichallenge_system.launch.xml >autoware.log 2>&1 &
