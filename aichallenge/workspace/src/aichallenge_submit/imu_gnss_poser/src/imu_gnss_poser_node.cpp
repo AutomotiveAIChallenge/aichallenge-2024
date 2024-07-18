6a3996f521f33310659f356c3ca7906d92cbc3f8
taikitanaka3 <65527974+taikitanaka3@users.noreply.github.com>
2024-07-18 13:18:57 +0900

    fix: correct localization (#24)
    * fix: ekf initialize
    
    * chore: add gyro odmetor
    
    * feat: add imu corrector
    
    * fix: covariance for pose
    
    * revert: pure pursuit and fix: imu corrector launch
    
    * fix: imu name space
    
    * chore: add pkill script for developer
    
    * fix: sensor kit param
    
    * fix: gyro odometor angular x, y to zero
    
    * fix: sensor kit calibration param
    
    * chore: set better pp param
    
    * revert: gyro stdddev
    
    * chore: change covariance to zero
    
    * chore: fix ll2

--- aichallenge/workspace/src/aichallenge_submit/imu_gnss_poser/src/imu_gnss_poser_node.cpp
+++ aichallenge/workspace/src/aichallenge_submit/imu_gnss_poser/src/imu_gnss_poser_node.cpp
@@ -1,5 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
+#include "geometry_msgs/msg/twist_with_covariance_stamped.hpp"
 #include "sensor_msgs/msg/imu.hpp"
 
 class ImuGnssPoser : public rclcpp::Node
@@ -10,6 +11,10 @@ public:
     {
         auto qos = rclcpp::QoS(rclcpp::KeepLast(10)).reliable();
         pub_pose_ = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("/localization/imu_gnss_poser/pose_with_covariance", qos);
+        pub_initial_pose_3d_ = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("/localization/initial_pose3d", qos);
+        sub_twist_ = this->create_subscription<geometry_msgs::msg::TwistWithCovarianceStamped>(
+            "/localization/twist_with_covariance", qos,
+            std::bind(&ImuGnssPoser::twist_callback, this, std::placeholders::_1)); 
         sub_gnss_ = this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
             "/sensing/gnss/pose_with_covariance", qos,
             std::bind(&ImuGnssPoser::gnss_callback, this, std::placeholders::_1));
@@ -25,26 +30,36 @@ private:
         msg->pose.pose.orientation.y = imu_msg_.orientation.y;
         msg->pose.pose.orientation.z = imu_msg_.orientation.z;
         msg->pose.pose.orientation.w = imu_msg_.orientation.w;
-        msg->pose.covariance[7*0] = 10.0;
-        msg->pose.covariance[7*1] = 10.0;
-        msg->pose.covariance[7*2] = 10.0;
-        msg->pose.covariance[7*3] = 0.1;
-        msg->pose.covariance[7*4] = 0.1;
-        msg->pose.covariance[7*5] = 1.0;
+        msg->pose.covariance[7*0] = 0.1;
+        msg->pose.covariance[7*1] = 0.1;
+        msg->pose.covariance[7*2] = 0.1;
+        msg->pose.covariance[7*3] = 0.01;
+        msg->pose.covariance[7*4] = 0.01;
+        msg->pose.covariance[7*5] = 0.1;
         pub_pose_->publish(*msg);
+        if (!is_ekf_initialized_)
+            pub_initial_pose_3d_->publish(*msg);
     }
 
     void imu_callback(sensor_msgs::msg::Imu::SharedPtr msg) {
         imu_msg_ = *msg;
     }
 
+    void twist_callback(const geometry_msgs::msg::TwistWithCovarianceStamped::SharedPtr msg)
+    {
+        is_ekf_initialized_ = true;
+    }
+
     rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pub_pose_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pub_initial_pose_3d_;
     rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr sub_gnss_;
+    rclcpp::Subscription<geometry_msgs::msg::TwistWithCovarianceStamped>::SharedPtr sub_twist_;
     rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr sub_imu_;
     sensor_msgs::msg::Imu imu_msg_;
+    bool is_ekf_initialized_ = {false};
 };
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<ImuGnssPoser>());
