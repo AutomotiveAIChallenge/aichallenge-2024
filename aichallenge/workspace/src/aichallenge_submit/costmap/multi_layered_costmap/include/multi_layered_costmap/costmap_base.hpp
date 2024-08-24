a65f29ecc6b5ebe90fefc616fdbdee1fb4d4894c
Autumn60 <harada.akiro@gmail.com>
2024-08-24 15:13:24 +0900

    fix license notation
    Signed-off-by: Autumn60 <harada.akiro@gmail.com>

--- aichallenge/workspace/src/aichallenge_submit/costmap/multi_layered_costmap/include/multi_layered_costmap/costmap_base.hpp
+++ aichallenge/workspace/src/aichallenge_submit/costmap/multi_layered_costmap/include/multi_layered_costmap/costmap_base.hpp
@@ -1,4 +1,4 @@
-// Copyright 2024 Fool Stuck Engineers
+// Copyright 2024 Booars
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
