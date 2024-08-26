1fa1fcc976b72e7511a25f8777c6f1842514ab19
taikitanaka3 <taiki.tanaka@tier4.jp>
2024-08-26 18:49:48 +0900

    feat: add source build command

--- aichallenge/workspace/colcon_build.bash
+++ aichallenge/workspace/colcon_build.bash
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-colcon build --symlink-install --packages-up-to aichallenge_launch
+colcon build --symlink-install --packages-up-to aichallenge_launch --parallel-workers 1 --executor sequential --cmake-args -DCMAKE_BUILD_TYPE=Release
