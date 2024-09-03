ef627b7cf5c261b17d6fabfdf59b18d81196bf41
Masahiro Kubota <norikenpi@gmail.com>
2024-09-03 21:16:03 +0900

    fix: modify build command
    Signed-off-by: Masahiro Kubota <norikenpi@gmail.com>

--- aichallenge/workspace/colcon_build.bash
+++ aichallenge/workspace/colcon_build.bash
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-MAKEFLAGS=-j4 colcon build --symlink-install --packages-up-to aichallenge_launch --executor sequential
+MAKEFLAGS=-j4 colcon build --symlink-install --packages-up-to aichallenge_launch --executor sequential --cmake-args -DCMAKE_BUILD_TYPE=Release
