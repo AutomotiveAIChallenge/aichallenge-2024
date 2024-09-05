e3cd2f614982e98a846631ab9ba0f5afc71fe0df
taikitanaka3 <taiki.tanaka@tier4.jp>
2024-09-05 22:43:30 +0900

    chore: add better build script

--- aichallenge/workspace/colcon_build.bash
+++ aichallenge/workspace/colcon_build.bash
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-MAKEFLAGS=-j4 colcon build --symlink-install --packages-up-to aichallenge_launch --executor sequential --cmake-args -DCMAKE_BUILD_TYPE=Release
+MAKEFLAGS="-j4 -l 3.0" colcon build --symlink-install --packages-up-to aichallenge_launch --cmake-args -DCMAKE_BUILD_TYPE=Release
