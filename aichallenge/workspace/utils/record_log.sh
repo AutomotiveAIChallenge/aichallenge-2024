ec8e9851f6278ab9c48f939fc019ccdbb97a4ebb
Masahiro Kubota <42679530+masahiro-kubota@users.noreply.github.com>
2024-09-09 08:01:46 +0900

    feat: add the script recording rosbag and ros parameter (#81)
    Signed-off-by: Masahiro Kubota <norikenpi@gmail.com>

---
+++ aichallenge/workspace/utils/record_log.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+NODES_TO_RECORD=("localization/ekf_localizer" "simple_pure_pursuit_node") 
+
+source $HOME/aichallenge-2024/aichallenge/workspace/install/setup.bash
+source $HOME/racing_kart_interface/install/setup.bash
+
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+
+RECORD_DIR="$HOME/rosbag/$(date +%m%d)/$TIMESTAMP"
+
+mkdir -p "$RECORD_DIR"
+
+for node in "${NODES_TO_RECORD[@]}"; do
+    ros2 param dump "/$node" >> "$RECORD_DIR/node_param.yaml"
+    echo "Recorded parameters for /$node"
+done
+
+ros2 bag record -a -o "$RECORD_DIR/$TIMESTAMP"
+
+echo "Rosbag recording finished. Compressing data..."
+
+cd "$RECORD_DIR" || exit
+
+zip -r "$RECORD_DIR.zip" "./"
+
+rm -r "$RECORD_DIR"
+
+echo "Compression completed. ZIP file created at $RECORD_DIR/${TIMESTAMP}_rosbag_and_params.zip"
