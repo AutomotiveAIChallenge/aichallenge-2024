a20cdee9f98900d0aeb696b041c1ecae545368fd
taikitanaka3 <taiki.tanaka@tier4.jp>
2024-09-17 18:47:38 +0900

    chore: add lauch rviz arg

--- aichallenge/workspace/run_aichallenge_launch_run_vehicle.sh
+++ aichallenge/workspace/run_aichallenge_launch_run_vehicle.sh
@@ -2,4 +2,4 @@
 
 source install/setup.bash
 
-ros2 launch aichallenge_system_launch aichallenge_system.launch.xml simulation:=false use_sim_time:=false
+ros2 launch aichallenge_system_launch aichallenge_system.launch.xml simulation:=false use_sim_time:=false run_rviz:=true
