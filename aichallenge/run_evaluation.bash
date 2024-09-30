ad91b58bca1143923564618eb9a8061627eb2550
Yu Asabe <yu.asabe@tier4.jp>
2024-09-30 18:32:21 +0900

    fix: minor updates to fix run_evaluation.bash script (#119)
    * fix: add ScreenCapturePanel to debug_sensing.rviz
    
    Signed-off-by: Yu Asabe <yu.asabe@tier4.jp>
    
    * fix: set initial pose in run_evaluation.bash
    
    Signed-off-by: Yu Asabe <yu.asabe@tier4.jp>
    
    ---------
    
    Signed-off-by: Yu Asabe <yu.asabe@tier4.jp>

--- aichallenge/run_evaluation.bash
+++ aichallenge/run_evaluation.bash
@@ -44,6 +44,30 @@ wmctrl -a "AWSIM" && wmctrl -r "AWSIM" -e 0,0,0,900,1043
 ros2 service call /debug/service/capture_screen std_srvs/srv/Trigger >/dev/null
 sleep 1
 
+# Set initial pose
+echo "Set initial pose"
+ros2 topic pub -1 /initialpose geometry_msgs/msg/PoseWithCovarianceStamped "{ 
+  header: {
+    frame_id: 'map'
+  },
+  pose: {
+    pose: {
+      position: {
+        x: 89633.29,
+        y: 43127.57,
+        z: 0.0
+      },
+      orientation: {
+        x: 0.0,
+        y: 0.0,
+        z: 0.8778,
+        w: 0.4788
+      }
+    }
+  }
+}" >/dev/null
+sleep 1
+
 # Start driving and wait for the simulation to finish
 echo "Waiting for the simulation"
 ros2 service call /control/control_mode_request autoware_auto_vehicle_msgs/srv/ControlModeCommand '{mode: 1}' >/dev/null
