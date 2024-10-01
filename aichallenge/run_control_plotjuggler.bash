14550e1d2c617fcb9dc69bcd04f72cce1b8e71dc
knorrrr <99851410+knorrrr@users.noreply.github.com>
2024-10-01 09:43:04 +0900

    chore: add bash script (#7)

---
+++ aichallenge/run_control_plotjuggler.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+source /aichallenge/workspace/install/setup.bash
+ros2 run plotjuggler plotjuggler -l /aichallenge/workspace/src/aichallenge_submit/aichallenge_submit_launch/config/control/plot_juggler_trajectory_follower.xml
---
+++ aichallenge/run_localizer_plotjuggler.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+source /aichallenge/workspace/install/setup.bash
+ros2 run plotjuggler plotjuggler -l /aichallenge/workspace/src/aichallenge_submit/aichallenge_submit_launch/config/localizer_plotjuggler.xml 
