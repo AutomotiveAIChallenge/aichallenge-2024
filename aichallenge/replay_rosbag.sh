13df73aa0eb6553ca7f1d1febab9ee69a45dc457
taikitanaka3 <taiki.tanaka@tier4.jp>
2024-10-03 12:18:52 +0900

    chore: add script

--- aichallenge/replay_rosbag.sh
+++ aichallenge/replay_rosbag.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-source install/setup.bash
+# shellcheck disable=SC1091
+source /aichallenge/workspace/install/setup.bash
+sudo ip link set multicast on lo
 
 ros2 launch aichallenge_system_launch aichallenge_system.launch.xml simulation:=false use_sim_time:=true run_rviz:=true
---
+++ aichallenge/run_vehicle.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+source /aichallenge/workspace/install/setup.bash
+sudo ip link set multicast on lo
+
+ros2 launch aichallenge_system_launch aichallenge_system.launch.xml simulation:=false
