28ddbf81bfcded1cc603819c00377cd30a563496
knorrrr <kunotomoki61@gmail.com>
2024-10-17 20:44:50 +0900

    set env var

--- vehicle/run_zenoh.bash
+++ vehicle/run_zenoh.bash
@@ -1,7 +1,5 @@
 #!/bin/bash
-# shellcheck disable=SC2086
 
-rocker --x11 --devices /dev/dri --user --net host --privileged --name zenoh --volume aichallenge:/aichallenge -- "aichallenge-2024-dev-${USER}"  bash -c "zenoh-bridge-ros2dds -c /vehicle/zenoh.json5"
-# rocker --x11 --devices /dev/dri --user --net host --privileged --name zenoh --volume aichallenge:/aichallenge -- "aichallenge-2024-dev-${USER}"  bash -c "ROS_DISTRO=humble zenoh-bridge-ros2dds -c /vehicle/zenoh.json5"
-# rocker --x11 --devices /dev/dri --user --net host --privileged --name zenoh --volume ../aichallenge:/aichallenge -- "aichallenge-2024-dev-${USER}"  
+# shellcheck disable=SC2086
+rocker --x11 --devices /dev/dri --env ROS_DISTRO=humble --user --net host --privileged --name zenoh --volume aichallenge:/aichallenge -- "aichallenge-2024-dev-${USER}" zenoh-bridge-ros2dds -c /vehicle/zenoh.json5
 
