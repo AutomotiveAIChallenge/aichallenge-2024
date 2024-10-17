5b2ade094bf6aab267e7535d3f487ebb1a74e41e
knorrrr <kunotomoki61@gmail.com>
2024-10-17 20:45:49 +0900

    appky precommit

--- vehicle/run_zenoh.bash
+++ vehicle/run_zenoh.bash
@@ -2,4 +2,3 @@
 
 # shellcheck disable=SC2086
 rocker --x11 --devices /dev/dri --env ROS_DISTRO=humble --user --net host --privileged --name zenoh --volume aichallenge:/aichallenge -- "aichallenge-2024-dev-${USER}" zenoh-bridge-ros2dds -c /vehicle/zenoh.json5
-
