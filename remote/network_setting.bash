e6bf1cdbe0f8d40e1b8eb1cc8e497698eca9b51b
Masahiro Kubota <norikenpi@gmail.com>
2024-10-18 12:59:04 +0900

    fix: change the buffer size accroding to awf document.
    Signed-off-by: Masahiro Kubota <norikenpi@gmail.com>

--- remote/network_setting.bash
+++ remote/network_setting.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sudo sysctl -w net.core.rmem_max=10485760
-sudo sysctl -w net.core.rmem_default=10485760
+sudo sysctl -w net.core.rmem_max==2147483647
 sudo ip link set lo multicast on
