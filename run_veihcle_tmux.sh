92b2d0adcbdec9da93a527727a2cf961fae4d0b3
Masahiro Kubota <norikenpi@gmail.com>
2024-10-18 12:04:40 +0900

    revert: delete host network setting
    Signed-off-by: Masahiro Kubota <norikenpi@gmail.com>

--- run_veihcle_tmux.sh
+++ run_veihcle_tmux.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
 bash stop_vehicle_tmux.sh
-
-sudo sysctl -w net.core.rmem_max=10485760
-sudo sysctl -w net.core.rmem_default=10485760
-sudo ip link set lo multicast on
-
 tmux new-session \; source-file run_script.tmux
