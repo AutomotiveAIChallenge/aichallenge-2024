801598be9cbc72b526139ef671afdc34b7d14bc4
tomoki <kunotomoki61@gmail.com>
2024-10-27 03:01:10 +0900

    mv rosbag and gitignore

--- aichallenge/run_replay_rosbag_autoware.sh
+++ aichallenge/run_replay_rosbag_autoware.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 source /aichallenge/workspace/install/setup.bash
-ros2 bag play /aichallenge/workspace/src/rosbag_data/0920_demo_interface_only_02 -l --clock &
+ros2 bag play /aichallenge/workspace/rosbag/0920_demo_interface_only_02 --topics /control/command/actuation_cmd /control/command/control_cmd /sensing/gnss/pose_with_covariance /sensing/gnss/pose /sensing/imu/imu_raw /vehicle/status/actuation_status /vehicle/status/control_mode /vehicle/status/gear_status /vehicle/status/steering_status /vehicle/status/velocity_status --clock &
 ros2 launch aichallenge_system_launch aichallenge_system.launch.xml simulation:=false use_sim_time:=true run_rviz:=true is_rosbag:=true
