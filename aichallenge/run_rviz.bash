597ae629142457455888bcd4aefdd1aabbc8faf2
taikitanaka3 <taiki.tanaka@tier4.jp>
2025-04-05 23:40:34 +0900

    style: add fix

--- aichallenge/run_rviz.bash
+++ aichallenge/run_rviz.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC1091
 source /aichallenge/workspace/install/setup.bash
 rviz2 -d /aichallenge/workspace/src/aichallenge_system/aichallenge_system_launch/config/autoware.rviz
 # rviz2 -d /aichallenge/workspace/src/aichallenge_system/aichallenge_system_launch/config/debug_sensing.rviz
