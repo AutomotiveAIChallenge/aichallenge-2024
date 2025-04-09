89bd6fd07bffc04aefffc371a3b23ba69df2a070
knorrrr <kunotomoki61@gmail.com>
2025-04-09 13:58:22 +0900

    feat: update exec simply

--- docker_exec.sh
+++ docker_exec.sh
@@ -20,7 +20,35 @@ docker ps --format "{{.ID}}\t{{.Names}}" | nl -w2 -s') '
 echo "--------------------------------------"
 echo ""
 
+# Count number of running containers
+CONTAINER_COUNT=$(echo "$RUNNING_CONTAINERS" | wc -l)
+
+# If only one container is running, select it automatically
+if [ "$CONTAINER_COUNT" -eq 1 ]; then
+    SELECTED_CONTAINER=$(docker ps -q)
+    CONTAINER_NAME=$(docker ps --format "{{.Names}}")
+    echo "Only one container running: $CONTAINER_NAME"
+    echo "Automatically connecting to this container..."
+    docker exec -it "$SELECTED_CONTAINER" bash
+    exit 0
+fi
+
+
 # Ask user to select a container by number
-echo "Enter the ID of the container you want to execute a command in:"
-read -r SELECTED_CONTAINER
+echo "Enter the number of the container you want to execute a command in:"
+read -r CONTAINER_NUM
+
+# Get the container ID from the selected number
+SELECTED_CONTAINER=$(docker ps --format "{{.ID}}" | sed -n "${CONTAINER_NUM}p")
+
+if [ -z "$SELECTED_CONTAINER" ]; then
+    echo "Invalid selection. Exiting."
+    exit 1
+fi
+# Get the container name
+CONTAINER_NAME=$(docker ps --format "{{.Names}}" | sed -n "${CONTAINER_NUM}p")
+# Display which container we're connecting to
+echo "Connecting to container: $CONTAINER_NAME (ID: $SELECTED_CONTAINER)"
+
+# Execute bash in the selected container
 docker exec -it "$SELECTED_CONTAINER" bash
