a568e0013e683a44eaab1d8d65d726173a91642a
Knorrrr <99851410+knorrrr@users.noreply.github.com>
2025-04-19 15:22:53 +0900

    chore: change dockerfile and docker_run.sh (#169)
    * dockerfile copy to mount. auto detect nvidia
    
    * change permission to 757 and file-extension
    
    * enable select gpu
    
    * bug fix

--- docker_run.sh
+++ docker_run.sh
@@ -2,13 +2,12 @@
 
 target="${1}"
 device="${2}"
-
 case "${target}" in
 "eval")
     volume="output:/output"
     ;;
 "dev")
-    volume="output:/output aichallenge:/aichallenge"
+    volume="output:/output aichallenge:/aichallenge remote:/remote vehicle:/vehicle"
     ;;
 *)
     echo "invalid argument (use 'dev' or 'eval')"
@@ -16,20 +15,21 @@ case "${target}" in
     ;;
 esac
 
-case "${device}" in
-"cpu")
+if [ "${device}" = "cpu" ]; then
     opts=""
-    ;;
-"gpu")
+    echo "[INFO] Running in CPU mode (forced by argument)"
+elif [ "${device}" = "gpu" ]; then
     opts="--nvidia"
-    ;;
-*)
-    echo "invalid argument (use 'gpu' or 'cpu')"
-    exit 1
-    ;;
-esac
+    echo "[INFO] Running in GPU mode (forced by argument)"
+elif command -v nvidia-smi &>/dev/null && [[ -e /dev/nvidia0 ]]; then
+    opts="--nvidia"
+    echo "[INFO] NVIDIA GPU detected → enabling --nvidia"
+else
+    opts=""
+    echo "[INFO] No NVIDIA GPU detected → running on CPU"
+fi
 
 mkdir -p output
 
 # shellcheck disable=SC2086
-rocker "${opts}" --x11 --devices /dev/dri --user --net host --privileged --name "aichallenge-2024-$(date "+%Y-%m-%d-%H-%M-%S")" --volume ${volume} -- "aichallenge-2024-${target}-${USER}"
+rocker ${opts} --x11 --devices /dev/dri --user --net host --privileged --name "aichallenge-2024-$(date "+%Y-%m-%d-%H-%M-%S")" --volume ${volume} -- "aichallenge-2024-${target}-${USER}"
