0b70712ddb8d7a2c0f65303957d85cb2da6ad979
taikitanaka3 <taiki.tanaka@tier4.jp>
2025-05-01 12:15:48 +0900

    fix: zenoh

--- vehicle/run_zenoh.bash
+++ vehicle/run_zenoh.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # shellcheck disable=SC2086
-rocker --x11 --devices /dev/dri --env ROS_DISTRO=humble --user --net host --privileged --name zenoh --volume aichallenge:/aichallenge -- "aichallenge-2024-dev-${USER}" zenoh-bridge-ros2dds -c /vehicle/zenoh.json5
+rocker --x11 --devices /dev/dri --env ROS_DISTRO=humble --user --net host --privileged --name zenoh --volume aichallenge:/aichallenge vehicle:/vehicle -- "aichallenge-2024-dev-${USER}" zenoh-bridge-ros2dds -c /vehicle/zenoh.json5
